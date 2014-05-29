5b5158e6b4d9425bf90c8a02c9f4698ed02d5a80
MoroishiYuya <1201525@st.asojuku.ac.jp>
2014-05-29 10:55:48 +0900

    ログインテスト

--- Login2.php
+++ Login2.php
@@ -4,19 +4,11 @@
     <title>ログイン</title>
   </head>
   <body>
-    入力フォームです。
-    名前を入力してみましょう。
-    <form action="output.php" method="post">
-      <table border="1">
-        <tr>
-          <td>名前</td>
-          <td><input type="text" name="name"></td>
-          <td colspan="2" align="center"><input type="submit" value="入力"></td>
-        </tr>
-      </table>
-    </form>
     <?php
-
+      $m_code = $_POST['m_code'];
+      $m_pass = $_POST['m_pass']
+      print ("次のデータを受け取りました<br />");
+      print ("名前：$name<br />");
       $con = mysql_connect('172.20.17.202','admin','1111');
       if (!$con) {
         exit('データベースに接続できませんでした。');
@@ -32,7 +24,7 @@
         exit('文字コードを指定できませんでした。');
       }
 
-      $result = mysql_query('SELECT * FROM member', $con);
+      $result = mysql_query('SELECT count FROM member WHERE m_code = ' . $m_code . ' and m_pass = ' . $m_pass , $con);
       while ($data = mysql_fetch_array($result)) {
         echo '<p>' . $data['m_code'] . '←会員コード　' . $data['m_pass'] . '←パスワード　' . $data['m_name'] . '←名前　' . $data['m_add'] . '←住所　' . $data['m_tel'] . '←電話番号　' . $data['m_mail'] .'←メールアドレス　'."</p>\n";
       }
@@ -43,5 +35,16 @@
       }
 
     ?>
+    入力フォームです。
+    名前を入力してみましょう。
+    <form action="output.php" method="post">
+      <table border="1">
+        <tr>
+          <td>名前</td>
+          <td><input type="text" name="name"></td>
+          <td colspan="2" align="center"><input type="submit" value="入力"></td>
+        </tr>
+      </table>
+    </form>
   </body>
 </html>
