ae71220a72fab8e6a818e27969426465033a0cac
MoroishiYuya <1201525@st.asojuku.ac.jp>
2014-06-03 12:06:03 +0900

    作成中画面

--- SMemberMenu.php
+++ SMemberMenu.php
@@ -4,10 +4,9 @@
     <title>販売会員メインメニュー</title>
   </head>
   <body>
-    <p>ログイン失敗</p>
+    <p><a href="http://www.yahoo.co.jp/">商品登録</a></p>
+    <p><a href="http://www.yahoo.co.jp/">商品情報登録</a></p>
+    <p><a href="http://www.yahoo.co.jp/">注文履歴確認</a></p>
+    <p><a href="http://www.yahoo.co.jp/">販売会員の情報更新</a></p>
   </body>
 </html>
-商品登録
-商品情報登録
-注文履歴確認
-販売会員の情報更新
---
+++ mada.php
@@ -0,0 +1,9 @@
+﻿<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>まだ完成していないページです</title>
+  </head>
+  <body>
+    <p>このページは作成中です<img src="era.png"></p>
+  </body>
+</html>
