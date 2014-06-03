775ea5cc0a09393f5245799bfa5c2cb521cd7fc1
MoroishiYuya <1201525@st.asojuku.ac.jp>
2014-06-03 12:07:51 +0900

    画像サイズ変更

--- mada.php
+++ mada.php
@@ -4,6 +4,6 @@
     <title>まだ完成していないページです</title>
   </head>
   <body>
-    <p>このページは作成中です<img src="era.png"></p>
+    <p>このページは作成中です<img src="era.png" width="50%" height="50%"></p>
   </body>
 </html>
