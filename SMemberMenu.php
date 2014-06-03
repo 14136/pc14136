0f1ede98e243c972e9e15b44667b184ab412fa8b
MoroishiYuya <1201525@st.asojuku.ac.jp>
2014-06-03 12:22:25 +0900

    リンク先変更

--- SMemberMenu.php
+++ SMemberMenu.php
@@ -4,9 +4,9 @@
     <title>販売会員メインメニュー</title>
   </head>
   <body>
-    <p><a href="http://www.yahoo.co.jp/">商品登録</a></p>
-    <p><a href="http://www.yahoo.co.jp/">商品情報登録</a></p>
-    <p><a href="http://www.yahoo.co.jp/">注文履歴確認</a></p>
-    <p><a href="http://www.yahoo.co.jp/">販売会員の情報更新</a></p>
+    <p><a href="http://172.20.17.202/kome/mada.php">商品登録</a></p>
+    <p><a href="http://172.20.17.202/kome/mada.php">商品情報登録</a></p>
+    <p><a href="http://172.20.17.202/kome/mada.php">注文履歴確認</a></p>
+    <p><a href="http://172.20.17.202/kome/mada.php">販売会員の情報更新</a></p>
   </body>
 </html>
