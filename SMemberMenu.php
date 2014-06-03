04bdb5c25d998aa4d66b4984fb53197a3f7f4864
MoroishiYuya <1201525@st.asojuku.ac.jp>
2014-06-03 12:48:55 +0900

    セッション追加

--- SMemberMenu.php
+++ SMemberMenu.php
@@ -4,6 +4,9 @@
     <title>販売会員メインメニュー</title>
   </head>
   <body>
+    <?php
+      print("<p>" . $_SESSION['s_name'] . "さんようこそ</p>");
+    ?>
     <p><a href="http://172.20.17.202/kome/mada.php">商品登録</a></p>
     <p><a href="http://172.20.17.202/kome/mada.php">商品情報登録</a></p>
     <p><a href="http://172.20.17.202/kome/mada.php">注文履歴確認</a></p>
