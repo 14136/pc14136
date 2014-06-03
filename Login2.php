dad6f41c443722c27ce7ea7a8b4f10698212cd45
MoroishiYuya <1201525@st.asojuku.ac.jp>
2014-06-03 10:14:27 +0900

    test

--- Login2.php
+++ Login2.php
@@ -1,48 +1,30 @@
-<html>
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>ログイン</title>
-  </head>
-  <body>
-    <?php
-      $m_code = $_POST['m_code'];
-      $m_pass = $_POST['m_pass']
-      $con = mysql_connect('172.20.17.202','admin','1111');
-      if (!$con) {
-        exit('データベースに接続できませんでした。');
-      }
+<?php
+  $m_code = $_POST['m_code'];
+  $m_pass = $_POST['m_pass']
+  $con = mysql_connect('172.20.17.202','admin','1111');
+  if (!$con) {
+    exit('データベースに接続できませんでした。');
+  }
 
-      $result = mysql_select_db('riceshop', $con);
-      if (!$result) {
-        exit('データベースを選択できませんでした。');
-      }
+  $result = mysql_select_db('riceshop', $con);
+  if (!$result) {
+    exit('データベースを選択できませんでした。');
+  }
 
-      $result = mysql_query('SET NAMES utf8', $con);
-      if (!$result) {
-        exit('文字コードを指定できませんでした。');
-      }
+  $result = mysql_query('SET NAMES utf8', $con);
+  if (!$result) {
+    exit('文字コードを指定できませんでした。');
+  }
 
-      $result = mysql_query('SELECT count FROM member WHERE m_code = ' . $m_code . ' and m_pass = ' . $m_pass , $con);
-      while ($data = mysql_fetch_array($result)) {
-        echo '<p>' . $data['m_code'] . '←会員コード　' . $data['m_pass'] . '←パスワード　' . $data['m_name'] . '←名前　' . $data['m_add'] . '←住所　' . $data['m_tel'] . '←電話番号　' . $data['m_mail'] .'←メールアドレス　'."</p>\n";
-      }
+  $result = mysql_query("SELECT count FROM member WHERE m_code = $m_code and m_pass = $m_pass" , $con);
+  if ($result == 1){
+    header('Location: https://www.google.co.jp/
+  }else{
+    header('Location: http://172.20.17.202/kome/LoginFailure.php');
+  }
 
-      $con = mysql_close($con);
-      if (!$con) {
-        exit('データベースとの接続を閉じられませんでした。');
-      }
-
-    ?>
-    入力フォームです。
-    名前を入力してみましょう。
-    <form action="output.php" method="post">
-      <table border="1">
-        <tr>
-          <td>名前</td>
-          <td><input type="text" name="name"></td>
-          <td colspan="2" align="center"><input type="submit" value="入力"></td>
-        </tr>
-      </table>
-    </form>
-  </body>
-</html>
+  $con = mysql_close($con);
+  if (!$con) {
+    exit('データベースとの接続を閉じられませんでした。');
+  }
+?>
--- LoginFailure.php
+++ LoginFailure.php
@@ -1,1 +1,9 @@
-ログイン失敗
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>ログイン失敗</title>
+  </head>
+  <body>
+    <p>ログイン失敗</p>
+  </body>
+</html>
