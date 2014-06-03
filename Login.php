7d5ba73d5c1fd69befa64fa6f9cc55c343f36f2f
MoroishiYuya <1201525@st.asojuku.ac.jp>
2014-06-03 10:36:50 +0900

    ログイン変更

--- Login.php
+++ Login.php
@@ -5,6 +5,8 @@
   </head>
   <body>
     <p>会員コードとパスワードを入力してください</p>
+    <p>会員コード：3</p>
+    <p>パスワード：1111</p>
     <form action="Login2.php" method="post">
       <table border="1">
         <tr>
@@ -18,33 +20,5 @@
         </tr>
       </table>
     </form>
-    <?php
-
-      $con = mysql_connect('172.20.17.202','admin','1111');
-      if (!$con) {
-        exit('データベースに接続できませんでした。');
-      }
-
-      $result = mysql_select_db('riceshop', $con);
-      if (!$result) {
-        exit('データベースを選択できませんでした。');
-      }
-
-      $result = mysql_query('SET NAMES utf8', $con);
-      if (!$result) {
-        exit('文字コードを指定できませんでした。');
-      }
-
-      $result = mysql_query('SELECT * FROM member', $con);
-      while ($data = mysql_fetch_array($result)) {
-        echo '<p>' . $data['m_code'] . '←会員コード　' . $data['m_pass'] . '←パスワード　' . $data['m_name'] . '←名前　' . $data['m_add'] . '←住所　' . $data['m_tel'] . '←電話番号　' . $data['m_mail'] .'←メールアドレス　'."</p>\n";
-      }
-
-      $con = mysql_close($con);
-      if (!$con) {
-        exit('データベースとの接続を閉じられませんでした。');
-      }
-
-    ?>
   </body>
 </html>
