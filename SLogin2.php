fdd780dac2b686588def93e14c7536e30be29b1c
MoroishiYuya <1201525@st.asojuku.ac.jp>
2014-06-03 12:50:20 +0900

    セッション追加

--- SLogin2.php
+++ SLogin2.php
@@ -20,9 +20,9 @@
   $result = mysql_query("SELECT s_code,s_name,s_comp FROM s_member WHERE s_code = ". $s_code . " and s_pass = " . $s_pass , $con);
   if (mysql_num_rows($result) == 1){
     $row = mysql_fetch_assoc($result);
-    $_SESSION['s_code'] = $row['s_code']
-    $_SESSION['s_name'] = $row['s_name']
-    $_SESSION['s_comp'] = $row['s_comp']
+    $_SESSION['s_code'] = $row['s_code'];
+    $_SESSION['s_name'] = $row['s_name'];
+    $_SESSION['s_comp'] = $row['s_comp'];
     header('Location: http://172.20.17.202/kome/SMemberMenu.php');
   }else{
     header('Location: http://172.20.17.202/kome/LoginFailure.php');
