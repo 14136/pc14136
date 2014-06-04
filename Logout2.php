2c9ee05d77e1beccb8e38cbe6089ffd7ffa3dcb1
MoroishiYuya <1201525@st.asojuku.ac.jp>
2014-06-04 11:51:35 +0900

    ログアウト機能

---
+++ Logout2.php
@@ -0,0 +1,9 @@
+﻿<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>ログアウト</title>
+  </head>
+  <body>
+    <p>ログアウトしました</p>
+  </body>
+</html>
--- SMemberMenu.php
+++ SMemberMenu.php
@@ -8,9 +8,10 @@
       session_start();
       print("<p>".$_SESSION['s_name']."さんようこそ</p>");
     ?>
-    <p><a href="http://172.20.17.202/kome/mada.php">商品登録</a></p>
     <p><a href="http://172.20.17.202/kome/mada.php">商品情報登録</a></p>
+    <p><a href="http://172.20.17.202/kome/mada.php">商品情報更新</a></p>
     <p><a href="http://172.20.17.202/kome/mada.php">注文履歴確認</a></p>
     <p><a href="http://172.20.17.202/kome/mada.php">販売会員の情報更新</a></p>
+    <p><a href="http://172.20.17.202/kome/Logout.php">ログアウト</a></p>
   </body>
 </html>
