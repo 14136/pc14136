922a40d6cb75912012a3416a23ffabe654f366b6
MoroishiYuya <1201525@st.asojuku.ac.jp>
2014-06-10 11:44:46 +0900

    発送状態変更

--- DSChange.php
+++ DSChange.php
@@ -1,9 +1,48 @@
-﻿<html>
+﻿<?php
+      session_start();
+      $h_code = $_POST['h_code'];
+      $s_code = $_SESSION['s_code'];
+      $i = 0;
+
+
+      $con = mysql_connect('172.20.17.202', 'admin', '1111');
+      if(!$con){
+        exit('データベースに接続できませんでした。');
+      }
+      $result = mysql_select_db('riceshop', $con);
+        if(!$result){
+          exit('データベースを選択できませんでした。');
+        }
+      $result = mysql_query('SET NAMES utf8', $con);
+      if(!$result){
+        exit('文字コードを指定できませんでした。');
+      }
+
+      for($h_code[$i] < count($h_code)){
+        $sql = sprintf('UPDATE history SET h_pri = 1
+                        WHERE h_code = ' . $h_code[$i] );
+
+        $result_flag = mysql_query($sql);
+
+        if (!$result_flag) {
+        die('UPDATEクエリーが失敗しました。'.mysql_error());
+        }
+      }
+
+      $con = mysql_close($con);
+      if(!$con){
+        exit('データベースとの接続を閉じられませんでした。');
+      }
+
+
+?>
+<html>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>注文履歴確認</title>
   </head>
   <body>
+    <p>発送状態を変更しました</p>
     <form method="post" action="mada.php">
     <table border=1><tr><th></th><th>注文日</th><th>発送状態</th><th>顧客名</th><th>顧客住所</th><th>顧客TEL</th><th>顧客メールアドレス</th><th>注文商品</th><th>数量</th><th>単価</th><th>一点当たりの値引き額</th><th>請求金額</th></tr>
     <?php
@@ -33,7 +72,7 @@
                             "LEFT JOIN discount ON d_g_code = g_code AND h_date BETWEEN d_open AND d_end " .
                             "WHERE s_code =" . $s_code , $con);
       while($data = mysql_fetch_array($result)){
-        echo '<tr><td><input type="checkbox" name="h_code[]" value="' . $data['h_code'] . '">' . 
+        echo '<tr><td><input type="checkbox" name="h_code" value="' . $data['h_code'] . '">' . 
              '</td><td>' . $data['h_date'];
           if($data['h_pri'] == 0){
              echo '</td><td>未発送' ;
@@ -73,10 +112,6 @@
 
 
 <!--
-SELECT *
-FROM goods INNER JOIN s_member ON g_s_code = s_code
-INNER JOIN history ON g_code = h_g_code
-LEFT JOIN member ON m_code = h_m_code
-LEFT JOIN discount ON d_g_code = g_code AND h_date BETWEEN d_open AND d_end
-WHERE s_code = 1;
+UPDATE history SET h_pri
+WHERE h_code = 1;
 -->
