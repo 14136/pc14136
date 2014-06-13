78da94463f5cf7c8f54d7faa8ab9512718c847be
MoroishiYuya <1201525@st.asojuku.ac.jp>
2014-06-13 11:46:55 +0900

    変更

--- MIRegistration2.php
+++ MIRegistration2.php
@@ -25,7 +25,7 @@
 
       // ファイル取得
       $imgdat = file_get_contents($upfile);
-      $imgdat = mysql_real_escape_string($imgdat);
+      //$imgdat = mysql_real_escape_string($imgdat);
 
 
       $result = mysql_query('INSERT INTO goods (g_code,g_s_code,g_name,g_exp,g_phot,g_pri)' .
