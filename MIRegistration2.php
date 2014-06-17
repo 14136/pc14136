7e351df3a963a8d8e6cda8ae775b738374014baa
MoroishiYuya <1201525@st.asojuku.ac.jp>
2014-06-17 10:44:00 +0900

    大幅変更

--- MIRegistration2.php
+++ MIRegistration2.php
@@ -13,27 +13,16 @@
       if(!$result){
         exit('文字コードを指定できませんでした。');
       }
-      
 
 
+      $image = file_get_contents($_FILES['g_phot']);
 
-      $upfile = $_FILES['g_phot']['tmp_name'];
-      if ($upfile==""){
-        print("ファイルのアップロードができませんでした。<BR>\n");
-        exit;
-      }
-
-      // ファイル取得
-      $imgdat = file_get_contents($upfile);
-      //$imgdat = mysql_real_escape_string($imgdat);
-
-
-      $result = mysql_query("INSERT INTO goods (g_s_code,g_name,g_exp,g_phot,g_pri) " .
-                             "VALUES ('" . $_SESSION['s_code'] . "','" . 
-                                               $_POST['g_name'] . "','" . 
-                                               $_POST['g_exp'] . "','" . 
-                                               $imgdat . "," . 
-                                               $_POST['g_pri'] . "')");
+      $result = mysql_query('INSERT INTO goods (g_code,g_s_code,g_name,g_exp,g_phot,g_pri)' .
+                             'VALUES (null,' . $_SESSION['s_code'] . ',' . 
+                                               $_POST['g_name'] . ',' . 
+                                               $_POST['g_exp'] . ',' . 
+                                               $image . ',' . 
+                                               $_POST['g_pri'] . ')');
       if (!$result) {
         die('クエリーが失敗しました。'.mysql_error());
       }
