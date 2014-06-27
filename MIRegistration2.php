a2714ba13c6cedc85ddd72206c829491fbeb7066
MoroishiYuya <1201525@st.asojuku.ac.jp>
2014-06-27 12:39:34 +0900

    no message

--- MIRegistration2.php
+++ MIRegistration2.php
@@ -4,7 +4,7 @@
       $g_name = $_POST['g_name'];
       $g_exp = $_POST['g_exp'];
       $g_pri = $_POST['g_pri'];
-      $aaa = 0;
+      $aaa = 'gazou/noimage.jpg';
 
       $con = mysql_connect('172.20.17.202', 'admin', '1111');
       if(!$con){
@@ -35,35 +35,34 @@
       }else{
         print('クエリーが成功しました。');
       }
+
+
+
       
       $_SESSION['g_code'] = mysql_insert_id();
-      $fileinfo = pathinfo($_FILES["g_phot"]["name"]);
-      $fileext = strtoupper($fileinfo["extension"]);
-      $gazoumei = $_SESSION['g_code'] . '.' . $fileext;
-      $_FILES["g_phot"]["name"] = $gazoumei;
-if (is_uploaded_file($_FILES["g_phot"]["tmp_name"])) {
-  if (move_uploaded_file($_FILES["g_phot"]["tmp_name"], "gazou/" . $_FILES["g_phot"]["name"])) {
-    chmod("gazou/" . $_FILES["g_phot"]["name"], 0644);
-    echo $_FILES["g_phot"]["name"] . "をアップロードしました。";
-  } else {
-    echo "ファイルをアップロードできません。";
-  }
-} else {
-  echo "ファイルが選択されていません。";
-}
+      if (is_uploaded_file($_FILES["g_phot"]["tmp_name"])) {
+        $fileinfo = pathinfo($_FILES["g_phot"]["name"]);
+        $fileext = strtoupper($fileinfo["extension"]);
+        $gazoumei = $_SESSION['g_code'] . '.' . $fileext;
+        $_FILES["g_phot"]["name"] = $gazoumei;
+      
+        if (move_uploaded_file($_FILES["g_phot"]["tmp_name"], "gazou/" . $_FILES["g_phot"]["name"])) {
+          chmod("gazou/" . $_FILES["g_phot"]["name"], 0644);
+          echo $_FILES["g_phot"]["name"] . "をアップロードしました。";
+        } else {
+          echo "ファイルをアップロードできません。";
+        }
 
-$result = mysql_query('UPDATE goods SET g_phot = "gazou/' . $gazoumei . '" WHERE g_code = ' . $_SESSION['g_code'] );
-          if (!$result) {
-            die('クエリーが失敗しました。'.mysql_error());
-          }
+        $result = mysql_query('UPDATE goods SET g_phot = "gazou/' . $gazoumei . '" WHERE g_code = ' . $_SESSION['g_code'] );
+        if (!$result) {
+          die('クエリーが失敗しました。'.mysql_error());
+        }
+      }
 
       $con = mysql_close($con);
       if(!$con){
         exit('データベースとの接続を閉じられませんでした。');
       }
       header('Location: http://172.20.17.202/kome/MIRegistration3.php');
-
-
-
 
 ?>
