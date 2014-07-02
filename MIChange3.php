0fec4feccfd14b7b266e7fdc611ef3672e614ebb
MoroishiYuya <1201525@st.asojuku.ac.jp>
2014-07-02 12:50:08 +0900

    no message

--- MIChange3.php
+++ MIChange3.php
@@ -74,5 +74,5 @@
       if(!$con){
         exit('データベースとの接続を閉じられませんでした。');
       }
-      header('Location: http://172.20.17.202/kome/MIChange4.php');
+      //header('Location: http://172.20.17.202/kome/MIChange4.php');
 ?>
