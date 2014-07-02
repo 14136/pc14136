19a70d569ef46f19970ce474a041a8f932628553
MoroishiYuya <1201525@st.asojuku.ac.jp>
2014-07-02 12:29:58 +0900

    no message

--- MIChange3.php
+++ MIChange3.php
@@ -44,7 +44,7 @@
         if (is_uploaded_file($_FILES["g_phot"]["tmp_name"])) {
           $fileinfo = pathinfo($_FILES["g_phot"]["name"]);
           $fileext = strtoupper($fileinfo["extension"]);
-          $kuku = $_SESSION["g_phot"];
+          $kuku = $_SESSION["g_photmae"];
           mb_internal_encoding("UTF-8");
           $koko = mb_substr($kuku, 7);
           $gazoumei = $koko . '.' . $fileext;
